a8caa067872e9a3938c320ed678cb5afb464caea
abhinandvk <abhinandvk41@gmail.com>
2024-03-05 07:09:56 +0530

    updated

--- src/screens/HomeScreen/orginzation/picker/index.tsx
+++ src/screens/HomeScreen/orginzation/picker/index.tsx
@@ -1,13 +1,9 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import styles from './styles';
 
 const Picker = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Picker</Text>
-    </View>
-  );
+  return <View style={styles.container} />;
 };
 
 export default Picker;
